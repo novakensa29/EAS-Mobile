7693dd75be7e315a2693671c5b4db3094cfc24f5
novakensa29 <nova.kensa.tif20@polban.ac.id>
2022-05-27 20:30:04 +0700

    cardview

--- app/src/main/java/com/example/news/Adapter.java
+++ app/src/main/java/com/example/news/Adapter.java
@@ -1,43 +1,95 @@
 package com.example.news;
 
-
-import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.news.models.Article;
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.AdapterHolder> {
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
+    private List<Article> articles;
     private Context context;
-    private List<GetData> dataList;
+    private OnItemClickListener onItemClickListener;
 
-    public Adapter(Context context, List<GetData> datalist){
+
+    public Adapter(List<Article> articles, Context context) {
+        this.articles = articles;
         this.context = context;
-        this.dataList = datalist;
     }
+
     @NonNull
     @Override
-    public Adapter.AdapterHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.cardview, parent, false);
+        return new ViewHolder(view, onItemClickListener);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Adapter.AdapterHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holders, int position) {
+        final ViewHolder holder = holders;
+        Article model = articles.get(position);
 
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.placeholder(Utils.getRandomDrawbleColor());
+        requestOptions.error(Utils.getRandomDrawbleColor());
+        requestOptions.diskCacheStrategy(DiskCacheStrategy.ALL);
+        requestOptions.centerCrop();
+
+        Glide.with(context)
+                .load(model.getUrlToImage())
+                .apply(requestOptions)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(holder.imageView);
+
+        holder.title.setText(model.getTitle());
+        holder.desc.setText(model.getDescription());
+        holder.published_ad.setText(model.getPublishedAt());
+        holder.author.setText(model.getAuthor());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return articles.size();
     }
 
-    public class AdapterHolder extends RecyclerView.ViewHolder {
-        public AdapterHolder(@NonNull View itemView) {
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener){
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public class ViewHolder<itemView> extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView title, desc, author, published_ad;
+        ImageView imageView;
+        OnItemClickListener onItemClickListener;
+        public ViewHolder(@NonNull View itemView, OnItemClickListener onItemClickListener) {
             super(itemView);
+
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+            desc = itemView.findViewById(R.id.desc);
+            imageView = itemView.findViewById(R.id.imageView);
+
+            this.onItemClickListener = onItemClickListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            onItemClickListener.onItemClick(v, getAdapterPosition());
         }
     }
+
 }
